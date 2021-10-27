a42ec1c82bade33fba61d43d1419953c0fe59db6
CarlJacobson <68129658+CarlJacobson@users.noreply.github.com>
2021-10-27 15:22:41 -0400

    Update SolvedRemover.py

--- SolvedRemover.py
+++ SolvedRemover.py
@@ -2,13 +2,15 @@ import os
 from pathlib import Path
 import shutil
 
+#Fetches list of all filepaths containing "solved"
 pathlist = Path().glob('**/solved')
+#Removes student solution files
 for path in pathlist:
-    #print(path)
     if (str(path)[16:19]).lower() == 'stu':
         print(path)
         shutil.rmtree(path)
 
+#Removes class time trackers and lesson plants
 pathlist = Path().glob('*/*')
 for path in pathlist:
     if str(path)[2:] == 'TimeTracker.xlsx':
